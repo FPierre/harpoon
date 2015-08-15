8096a3447f8a7be34fe2a8d1aef6c628db5f2ab6
FPierre <pflauder@gmail.com>
2015-08-15 14:18:50 +0200

    Delete de suppression d'une news

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get    '/news',     to: 'news#index'
+  delete '/news/:id', to: 'news#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
