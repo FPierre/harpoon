7a1c3c61b623326ff7124eabe91e00ad23726598
FPierre <pflauder@gmail.com>
2015-08-18 23:37:00 +0200

    Possibilite de supprimer une news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,9 @@
 class NewsController < ApplicationController
   def index
     @news = New.all
+
+    config = Rails.application.config_for(:crawler)
+    @categories = config['tags']
   end
 
   def destroy
