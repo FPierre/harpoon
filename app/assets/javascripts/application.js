e791279a436bfa2e7ec07745a1b4a65ab87807e4
FPierre <pflauder@gmail.com>
2015-08-20 19:50:18 +0200

    Initialisation du collapse menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,17 @@
 //= require react
 //= require react_ujs
 //= require components
+//= require main
 //= require_tree .
+
+// https://dribbble.com/shots/2191473-Questionnaire-Step/attachments/404768
+// https://dribbble.com/shots/2191473-Questionnaire-Step/attachments/404767
+
+// https://www.airpair.com/reactjs/posts/reactjs-a-guide-for-rails-developers#refactor-state-helpers
+// http://blog.arkency.com/2014/07/why-we-use-react-js-in-our-rails-projects/
+// https://hackhands.com/react-rails-tutorial/
+// http://codeloveandboards.com/blog/2014/09/10/rails-and-react-ii-a-real-use-case/
+// https://facebook.github.io/react/docs/getting-started.html
+// https://facebook.github.io/react/docs/multiple-components.html
+// https://zapier.com/engineering/react-js-tutorial-guide-gotchas/
+// https://scotch.io/tutorials/learning-react-getting-started-and-concepts
