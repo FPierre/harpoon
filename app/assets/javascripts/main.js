6babd90751ed73d7c9e40aca954541ff2e9269c1
FPierre <pflauder@gmail.com>
2015-08-30 22:17:29 +0200

    JS (raw) d'affichage des articles

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,4 +4,22 @@ jQuery(document).ready(function($) {
   $('.news-container li').hover(function() {
 
   });
+
+  // TODO Passer à Toggle
+  $('.half-height').click(function() {
+    $('.article').addClass('half-height');
+  });
+
+  $('.full-height').click(function() {
+    $('.article').removeClass('half-height'); // .addClass('full-height');
+  });
+
+  // http://tinysort.sjeiti.com/
+  $('.alpha-sort').click(function() {
+
+  });
+
+  $('.date-sort').click(function() {
+
+  });
 });
