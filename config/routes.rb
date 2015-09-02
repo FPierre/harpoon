c34560c93fb3df1e0a1045a9d405c767a9c803d8
FPierre <pflauder@gmail.com>
2015-09-02 22:08:46 +0200

    Route et page d'index vers les statistiques

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get    '/articles',     to: 'articles#index'
   delete '/articles/:id', to: 'articles#destroy'
 
+  get '/statistics', to: 'statistics#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
