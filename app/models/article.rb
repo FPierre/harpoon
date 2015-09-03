e4606e210f876fff4e3f29eac9d442499173bf12
FPierre <pflauder@gmail.com>
2015-09-03 20:51:09 +0200

    Methode de classe d'Article pour formater les donnes pour le graphique des categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,14 @@
 class Article < ActiveRecord::Base
-  scope :category, ->(category) { where(category: category) }
-  scope :tag, ->(tag) { where(tag: tag) }
+  scope :category, -> (category) { where(category: category) }
+  scope :tag, -> (tag) { where(tag: tag) }
 
   after_commit{ ArticleRelayJob.perform_later(self) }
+
+  def self.percent_categories
+    config  = Rails.application.config_for(:crawler)
+    percent = Array.new
+
+    config['categories'].keys.each{ |c| percent << { label: c, value: Article.category(c).size, color: '#CCC' }}
+    percent
+  end
 end
