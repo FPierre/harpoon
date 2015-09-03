951d3f8c2eab1c0979b2b3c620a3476f47d84a31
FPierre <pflauder@gmail.com>
2015-09-03 19:56:49 +0200

    Creation de la lib de mise en forme des donnees pour construire les graphiques

---
+++ lib/charts_builder.rb
@@ -0,0 +1,11 @@
+class ChartsBuilder
+  def format_for_pie_chart()
+    data = Array.new
+
+    inputs.each do |input|
+      data << { value: , color: , label: , highlight:  }
+    end
+
+    data
+  end
+end
