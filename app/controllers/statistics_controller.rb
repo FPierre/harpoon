044c0800d2a9518ac9ae71b2e47064b1737d68eb
FPierre <pflauder@gmail.com>
2015-09-03 20:51:37 +0200

    Simplification du Controller par le passage des methodes du Model

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,9 +1,5 @@
 class StatisticsController < ApplicationController
   def index
-
-
-    config = Rails.application.config_for(:crawler)
-    @articles = Article.all.order(created_at: :desc)
-    @categories = config['categories'].keys
+    @percent_categories = Article.percent_categories
   end
 end
