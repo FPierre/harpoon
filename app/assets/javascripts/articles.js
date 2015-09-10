cb004a3415264c2b410f3a4c5730d81d7d8df31d
FPierre <pflauder@gmail.com>
2015-09-10 15:50:00 +0200

    Affichage d'une modal lors d'un click long sur un article

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,10 +1,6 @@
 jQuery(document).ready(function($) {
   $('.button-collapse').sideNav();
 
-  $('.news-container li').hover(function() {
-
-  });
-
   // TODO Passer à Toggle
   $('.half-height').click(function() {
     $.ajax({
@@ -17,6 +13,39 @@ jQuery(document).ready(function($) {
       }
     });
   });
+
+  // TODO Swipe à gauche ou à droite pour delete un article
+
+  $('.article').on('press', function(e) {
+    // e.preventDefault();
+    // console.log(this, e);
+    $('#article-actions-modal .modal-content h4').text($('.title h3', this).text());
+    $('#article-actions-modal .modal-content p').text($('.title p', this).text());
+
+
+    $('#article-actions-modal').openModal();
+  });
+
+  $('.article').on('flick', function(e) {
+    e.preventDefault();
+    // console.log(this, e);
+  });
+
+
+
+
+  // $('.article .created-at').click(function() {
+  //   // TODO Trouver un moyen plus élégant de cherche .article parent
+  //   $article = $(this).parent().parent();
+
+  //   if ($article.hasClass('on-update')) {
+  //     $article.removeClass('on-update');
+  //   }
+  //   else {
+  //     $('.article.on-update').removeClass('on-update');
+  //     $article.addClass('on-update');
+  //   }
+  // });
 
   $('.full-height').click(function() {
     $.ajax({
