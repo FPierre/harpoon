395f43634761bd6ded24a943d1495cfdae8cf1d6
FPierre <pflauder@gmail.com>
2015-09-12 17:00:33 +0200

    Installation de Devise

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,14 @@
-class StatisticsController < ApplicationController
+class SettingsController < ApplicationController
+  before_action :set_categories
+
   def index
 
+  end
+
+  private
+
+  def set_categories
+    config = Rails.application.config_for(:crawler)
+    @categories = config['categories'].keys
   end
 end
