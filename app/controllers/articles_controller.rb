a500d1dcc3690ee3303028f742bc5efb7ba0f477
FPierre <pflauder@gmail.com>
2015-10-11 22:24:33 +0200

    Utilise AC Website plutot que des string

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
-  before_action :set_categories
+  before_action :set_categories, only: :index
+  before_action :set_websites, only: :index
 
   def index
-    @articles  = Article.paginate(page: params[:page], per_page: 20).order(created_at: :desc)
+    @articles  = Article.paginate(page: params[:page], per_page: 40).order(created_at: :desc)
     @half_size = cookies[:half_size]
   end
 
@@ -26,5 +27,9 @@ class ArticlesController < ApplicationController
   def set_categories
     config = Rails.application.config_for :crawler
     @categories = config['categories'].keys
+  end
+
+  def set_websites
+    @websites = Website.all
   end
 end
