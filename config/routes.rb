d380937d19ad9a9faffb3f1ac33f834b4f8afd19
FPierre <pflauder@gmail.com>
2015-10-29 22:15:42 +0100

    Route pour delete la collection Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  resources 'articles', only: [:index, :destroy]
+  delete '/articles/delete',          to: 'articles#destroy_all',      constraints: { only_ajax: true }
   post   '/articles/half-size-state', to: 'articles#half_size_state', constraints: { active: true|false, only_ajax: true }
+  resources 'articles', only: [:index, :destroy]
 
-  get '/settings',   to: 'settings#index'
-  get '/statistics', to: 'statistics#index'
+  get    '/settings',   to: 'settings#index'
+  get    '/statistics', to: 'statistics#index'
+
+  scope '/admin' do
+    resources 'categories' do
+      resources 'tags'
+    end
+  end
 
   root 'welcome#index'
 
